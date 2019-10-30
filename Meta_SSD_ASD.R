84ab0571c58d4633b706752a49e3397e3f04f4a5
Lindsay Oliver <lindsaydoliv@gmail.com>
2019-10-30 14:40:57 -0400

    Created R script for meta-analysis of SSD and ASD data using metafor pkg

---
+++ Meta_SSD_ASD.R
@@ -0,0 +1,9 @@
+
+library(metafor)
+
+# read in extracted data
+
+
+# calculate individual effect sizes
+# specifically, calculate standardized mean effect sizes (Hedge's g) from the means, SDs, and Ns for the SSD and ASD groups for each paper
+
