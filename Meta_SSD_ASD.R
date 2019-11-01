f39480edd6024657f66e5ddfd44fd7e0b211e456
Lindsay Oliver <lindsaydoliv@gmail.com>
2019-11-01 17:44:14 -0400

    Added code to generate effect sizes and run models.

--- Meta_SSD_ASD.R
+++ Meta_SSD_ASD.R
@@ -1,9 +1,32 @@
 
 library(metafor)
 
-# read in extracted data
-
+# read in data for lower level soc cog
+meta_data_low <- read.csv("/projects/loliver/Systematic_Review/Approved_Paper_Data_Lower.csv")
+meta_data_hi <- read.csv("/projects/loliver/Systematic_Review/Approved_Paper_Data_Higher.csv")
+meta_data_rmet <- read.csv("/projects/loliver/Systematic_Review/Approved_Paper_Data_RMET.csv")
 
 # calculate individual effect sizes
 # specifically, calculate standardized mean effect sizes (Hedge's g) from the means, SDs, and Ns for the SSD and ASD groups for each paper
+# vtype="LS" (the default) uses the usual large-sample approximation to compute the sampling variances
+# vtype="UB" provides unbiased estimates of the sampling variances
+
+# low without RMET
+eff_low <- escalc(measure="SMD", n1i=SSD.N, n2i=ASD.N, m1i=O1.SSD.Mean, m2i=O1.ASD.Mean, sd1i=O1.SSD.SD, 
+       sd2i=O1.ASD.SD, data = meta_data_low[meta_data_low$Outcome.1!="RMET",], vtype = "LS", append = FALSE, var.names=c("eff_size","var"))
+
+# fit 
+rma(yi=eff_size, vi=var, data = eff_low)
+
+
+eff_hi <- escalc(measure="SMD", n1i=SSD.N, n2i=ASD.N, m1i=O1.SSD.Mean, m2i=O1.ASD.Mean, sd1i=O1.SSD.SD, 
+                  sd2i=O1.ASD.SD, data = meta_data_hi, vtype = "LS", append = FALSE, var.names=c("eff_size","var"))
+
+rma(yi=eff_size, vi=var, data = eff_hi)
+
+
+eff_rmet <- escalc(measure="SMD", n1i=SSD.N, n2i=ASD.N, m1i=O1.SSD.Mean, m2i=O1.ASD.Mean, sd1i=O1.SSD.SD, 
+                  sd2i=O1.ASD.SD, data = meta_data_rmet, vtype = "LS", append = FALSE, var.names=c("eff_size","var"))
+
+rma(yi=eff_size, vi=var, data = eff_rmet)
 
